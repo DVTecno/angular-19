92e2e4ee754b03b0bf2f35a92c281e59374f5dbf
Diego Veras <verasdiegoca78@gmail.com>
2024-12-14 19:06:43 -0300

    Fix SVG dynamic attributes for gradient offset and fill in product rating stars

--- src/app/products/ui/product-card/product-card.component.ts
+++ src/app/products/ui/product-card/product-card.component.ts
@@ -11,7 +11,34 @@ import { CommonModule } from '@angular/common';
 })
 export class ProductCardComponent {
   product = input.required<Product>();
-  get fullStars() {
-    return Math.floor(this.product().rating.rate);
+  // get fullStars() {
+  //   return Math.floor(this.product().rating.rate);
+  // }
+
+  //  // Propiedad calculada para estrellas completas (amarillas)
+  //  get fullStarsArray(): undefined[] {
+  //   return Array(Math.floor(this.product().rating.rate)).fill(undefined);
+  // }
+
+  // // Propiedad calculada para estrellas vacías (grises)
+  // get emptyStarsArray(): undefined[] {
+  //   return Array(5 - Math.floor(this.product().rating.rate)).fill(undefined);
+  // }
+
+  get fullStarsArray(): undefined[] {
+    return Array(Math.floor(this.product().rating.rate)).fill(undefined);
+  }
+
+  get hasHalfStar(): boolean {
+    return this.product().rating.rate % 1 > 0;
+  }
+
+  get decimalPercentage(): number {
+    return (this.product().rating.rate % 1) * 100; // Parte decimal en porcentaje
+  }
+
+  get emptyStarsArray(): undefined[] {
+    const emptyStars = 5 - Math.floor(this.product().rating.rate) - (this.hasHalfStar ? 1 : 0);
+    return Array(emptyStars).fill(undefined);
   }
 }
