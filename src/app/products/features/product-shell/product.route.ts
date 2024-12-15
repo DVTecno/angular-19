0e6be2a0d8ed29bdd85defb76f7d9cab67e08424
Diego Veras <verasdiegoca78@gmail.com>
2024-12-15 14:39:04 -0300

    update product details and routes

--- src/app/products/features/product-shell/product.route.ts
+++ src/app/products/features/product-shell/product.route.ts
@@ -4,4 +4,7 @@ export default [
   {
     path: '', loadComponent: () => import('../product-list/product-list.component')
     },
+    {
+      path: 'product/:id', loadComponent: () => import('../product-details/product-details.component')
+      },
 ] as Routes;
