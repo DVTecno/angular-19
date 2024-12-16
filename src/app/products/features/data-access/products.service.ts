7fbfda2ac8b2c4a482e37a318e4f6313eaddb8b7
Diego Veras <verasdiegoca78@gmail.com>
2024-12-15 21:35:14 -0300

    Implement dynamic fake pagination

--- src/app/products/features/data-access/products.service.ts
+++ src/app/products/features/data-access/products.service.ts
@@ -3,10 +3,16 @@ import { BaseHttpService } from "../../../shared/data-access/base-http.service";
 import { Observable } from "rxjs";
 import { Product } from "../../../shared/interfaces/product.interface";
 
+const LIMIT = 5;
+
 @Injectable({providedIn: 'root'})
 export class ProductService extends BaseHttpService {
-  getProducts():Observable<Product[]> {
-    return this.http.get<any[]>(this.apiUrl)
+  getProducts(page: number):Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.apiUrl}`,{
+      params:{
+        limit: page * LIMIT,
+      },
+    });
   }
 }
 
--- src/app/products/features/data-access/products.state.service.ts
+++ src/app/products/features/data-access/products.state.service.ts
@@ -1,13 +1,14 @@
+import { Product } from './../../../shared/interfaces/product.interface';
 import { inject, Injectable } from "@angular/core";
-import { Product } from "../../../shared/interfaces/product.interface";
 import { signalSlice } from 'ngxtension/signal-slice';
 import { ProductService } from "./products.service";
-import { map } from "rxjs";
+import { catchError, map, of, startWith, Subject, switchMap } from "rxjs";
 
 
 interface State {
   products: Product[];
   status: 'loading' | 'success' | 'error';
+  page: number;
 }
 
 @Injectable()
@@ -16,13 +17,30 @@ export class ProductsStateService {
   private initialState: State = {
     products: [],
     status: 'loading' as const,
+    page: 1,
   };
+
+  changePage$ = new Subject<number>();
+
+  loadProducts$ = this.changePage$.pipe(
+    startWith(1),
+    switchMap((page) => this.productsService.getProducts(page)),
+    map((products) => ({ products, status: 'success' as const })),
+    catchError(() => {
+      return of({
+        product: [],
+        status: 'error' as const
+      });
+    })
+  );
 
   state = signalSlice({
     initialState: this.initialState,
     sources: [
-      this.productsService.getProducts()
-      .pipe(map((products) => ({ products, status: 'success' as const }))),
+      this.changePage$.pipe(
+        map((page) => ({ page, status: 'loading' as const})),
+      ),
+      this.loadProducts$
     ],
   });
 }
