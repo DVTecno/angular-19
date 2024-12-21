9c378a95bb9bd20b1ec5617b665f2726ac329222
Diego Veras <verasdiegoca78@gmail.com>
2024-12-20 21:28:11 -0300

    Enhance product card component with rating display and update cart button text

--- src/app/products/ui/product-card/product-card.component.ts
+++ src/app/products/ui/product-card/product-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, input, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Product } from '../../../shared/interfaces/product.interface';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './product-card.component.html',
   styles: ``,
 })
@@ -18,5 +19,22 @@ export class ProductCardComponent {
     event.stopPropagation();
     event.preventDefault();
     this.addToCart.emit(this.product());
+  }
+
+  get fullStarsArray(): undefined[] {
+    return Array(Math.floor(this.product().rating.rate)).fill(undefined);
+  }
+
+  get hasHalfStar(): boolean {
+    return this.product().rating.rate % 1 > 0;
+  }
+
+  get decimalPercentage(): number {
+    return (this.product().rating.rate % 1) * 100; // Parte decimal en porcentaje
+  }
+
+  get emptyStarsArray(): undefined[] {
+    const emptyStars = 5 - Math.floor(this.product().rating.rate) - (this.hasHalfStar ? 1 : 0);
+    return Array(emptyStars).fill(undefined);
   }
 }
